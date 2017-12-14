977cb5785cae6c8d165255e928c11d3cd86f500f
sivakarthick <sivakarthick308@gmail.com>
2017-12-14 22:46:40 +0530

    First commit

---
+++ app/src/main/java/com/example/sivak/alert_dialog_box/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.sivak.alert_dialog_box;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder A_builder = new AlertDialog.Builder(MainActivity.this);
+                A_builder.setMessage("Do you want to close the app !!!!!").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = A_builder.create();
+                alert.setTitle("Alert !!!");
+                alert.show();
+            }
+        });
+
+    }
+}
